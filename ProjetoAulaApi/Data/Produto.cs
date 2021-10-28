1e56fe9d273935919e81dc47474e89843dd6ebfb
FabricioMessias <fabriciomessias@live.com>
2021-10-28 11:58:06 -0300

    Projeto Aula API

---
+++ ProjetoAulaApi/Data/Produto.cs
@@ -0,0 +1,14 @@
+﻿namespace ProjetoAulaApi.Data
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }    
+
+        public decimal Preco { get; set; }
+        
+        public int Estoque { get; set; }   
+
+    }
+}
